6c1d645276a113b42ee71a577b78c8afeab29f4e
Markus Koestl <markus.koestl@student.tugraz.at>
2021-04-14 12:09:36 +0200

    QL-001 Added Splash and Login Screen Design [KM, PG]

---
+++ app/src/main/java/com/example/quizlet/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.quizlet
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+
+}
