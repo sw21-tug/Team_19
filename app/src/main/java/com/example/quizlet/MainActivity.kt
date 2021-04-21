d1da697d685aaa78e098d8e1dc428efcff4853ca
Gregor Potthast <potthast@student.tugraz.at>
2021-04-21 11:58:18 +0200

    QL-001 Added Start Screen [PG, KM]

--- app/src/main/java/com/example/quizlet/MainActivity.kt
+++ app/src/main/java/com/example/quizlet/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.quizlet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var login = findViewById(R.id.login) as Button
+        login.setOnClickListener{
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
 
     }
 }
