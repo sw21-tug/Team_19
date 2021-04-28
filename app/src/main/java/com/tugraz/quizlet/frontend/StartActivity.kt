507b525894b55e9272977886205d71658bf63870
Ronald Gigerl <ronald.gigerl@student.tugraz.at>
2021-04-28 11:52:21 +0200

    QL-010 Implemented fragment behavior [GR, PG]

--- app/src/main/java/com/tugraz/quizlet/frontend/StartActivity.kt
+++ app/src/main/java/com/tugraz/quizlet/frontend/StartActivity.kt
@@ -2,6 +2,8 @@ package com.tugraz.quizlet.frontend
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.tugraz.quizlet.R
 
 class StartActivity : AppCompatActivity()
@@ -9,6 +11,13 @@ class StartActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<StartFragment>(R.id.main_fragment_view)
+            }
+        }
 
 
     }
